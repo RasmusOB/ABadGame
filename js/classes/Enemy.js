f3d1d51f55cc75aea59f75982cd8b44ad64e46b2
Rasmus Olofsson <81038309+RasmusOB@users.noreply.github.com>
2021-10-26 12:47:41 +0000

    Added mechanics like simple movement to the Enemy class from another project of mine

--- js/classes/Enemy.js
+++ js/classes/Enemy.js
@@ -1,16 +1,37 @@
-class Enemy{
+class Enemy {
+
   constructor(){
-
+  
+    this.x = random(1, totalWidth)
+    this.y = random(1, totalHeight)
+    this.r = 30
+    this.c = 'red'
+    this.vel = 2
     
   }
-
-  movement(){
-
-
-  }
-
-  graphics() {
+  
+  movement() {
+    
+    circle(this.x, this.y, this.r, this.c)
+    
+    let d = distance(this.x, this.y, player.x, player.y) 
+    
+    if(d < player.r) {
+      death()
+      stopUpdate()
+    }
+  
+    if(player.x > this.x) {
+      this.x += this.vel
+    } else {
+      this.x -= this.vel
+    }
+    
+    if(player.y > this.y) {
+      this.y += this.vel
+    } else {
+      this.y -= this.vel
+    }
     
   }
-
 }
