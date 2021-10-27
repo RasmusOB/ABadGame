2ad9cd2181b7a6baa6ce959c6ab35de240a9cca3
Rasmus Olofsson Burman <rasmus_ob@outlook.com>
2021-10-27 16:46:26 +0200

    Added the death function and comments

--- js/classes/Enemy.js
+++ js/classes/Enemy.js
@@ -1,57 +1,57 @@
 class Enemy {
-
-  constructor(x, y){
-  
-    this.x = x
-    this.y = y
-    this.R = 30
-    this.COLOR = 'red'
-    this.VEL = 2
-    
-  }
-  
-  movement() {
-    
-    let dP1 = distance(this.x, this.y, player1.x, player1.y) 
-    let dP2 = distance(this.x, this.y, player2.x, player2.y)
-    
-    if(dP1 < player1.R) {
-      death()
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.R = 30;
+        this.COLOR = 'red';
+        this.VEL = 2;
     }
 
-    if(dP2 < player2.R) {
-      death()
-    }
-  
-    if(dP1 < dP2) {
-      if(player1.x > this.x) {
-        this.x += this.VEL
-      } else {
-        this.x -= this.VEL
-      }
+    kill() {
+        let dP1 = distance(this.x, this.y, player1.x, player1.y);
+        let dP2 = distance(this.x, this.y, player2.x, player2.y);
 
-      if(player1.y > this.y) {
-        this.y += this.VEL
-      } else {
-        this.y -= this.VEL
-      }
+        if (dP1 < player1.R) {
+            death();
+        }
 
-    } else {
-      if(player2.x > this.x) {
-        this.x += this.VEL
-      } else {
-        this.x -= this.VEL
-      }
-
-      if(player2.y > this.y) {
-          this.y += this.VEL
-        } else {
-          this.y -= this.VEL
+        if (dP2 < player2.R) {
+            death();
         }
     }
-  }
 
-  graphics() {
-    circle(this.x, this.y, this.R, this.COLOR)
-  }
+    movement() {
+        let dP1 = distance(this.x, this.y, player1.x, player1.y);
+        let dP2 = distance(this.x, this.y, player2.x, player2.y);
+
+        if (dP1 < dP2) {
+            if (player1.x > this.x) {
+                this.x += this.VEL;
+            } else {
+                this.x -= this.VEL;
+            }
+
+            if (player1.y > this.y) {
+                this.y += this.VEL;
+            } else {
+                this.y -= this.VEL;
+            }
+        } else {
+            if (player2.x > this.x) {
+                this.x += this.VEL;
+            } else {
+                this.x -= this.VEL;
+            }
+
+            if (player2.y > this.y) {
+                this.y += this.VEL;
+            } else {
+                this.y -= this.VEL;
+            }
+        }
+    }
+
+    graphics() {
+        circle(this.x, this.y, this.R, this.COLOR);
+    }
 }
--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,78 @@
+// ALL THE GRAPHICS IN THE GAME
 function graphics() {
+    fill(BG_COLOR);
 
-  fill(BG_COLOR)
+    player1.graphics();
+    player2.graphics();
 
-  player1.graphics()
-  player2.graphics()
+    enemy1.graphics();
+    enemy2.graphics();
+    enemy3.graphics();
 
-  enemy1.graphics()
-  enemy2.graphics()
-  enemy3.graphics()
-
+    enemy1.kill();
+    enemy2.kill();
+    enemy3.kill();
 }
 
-
-function logic() {
-
-  player1.movement(keyboard.w, keyboard.a, keyboard.s, keyboard.d)
-
-  player2.movement(keyboard.up, keyboard.left, keyboard.down, keyboard.right)
-
-  enemy1.movement()
-  enemy2.movement()
-  enemy3.movement()
-
-  enemy1.movement()
-  enemy2.movement()
-  enemy3.movement()
-
-}
-
+// THE FUNCTION THAT IS CALLED WHEN YOU DIE
 function death() {
-  fill('red')
-  stopUpdate()
+    let randomMessage = random(1, 10);
+    let txtSize = 100;
+    let deathMessage = '';
+
+    switch (randomMessage) {
+        case 1:
+            deathMessage = "you know you should'nt die right?";
+            break;
+        case 2:
+            deathMessage = 'you guys are losers';
+            break;
+        case 3:
+            deathMessage = 'what how bad can you be?';
+            break;
+        case 4:
+            deathMessage = 'this is too easy for me';
+            break;
+        case 5:
+            deathMessage = 'you guys is just bad';
+            break;
+        case 6:
+            deathMessage = 'i am sorry but.. HOW?';
+            break;
+        case 7:
+            deathMessage = 'you got no excuses now';
+            break;
+        case 8:
+            deathMessage = 'are you serious?';
+            break;
+        case 9:
+            deathMessage = 'the goal is not to die';
+            break;
+        case 10:
+            deathMessage = 'this is just sad';
+            break;
+        default:
+            deathMessage =
+                'if you find this comment you is AWESOME, please tell me if you liked the game. THE MAKER IS RasmusOB';
+    }
+
+    fill('red');
+    text(100, txtSize + txtSize, txtSize, deathMessage, '#000');
+
+    stopUpdate();
+}
+
+// OTHER LOGIC
+function logic() {
+    player1.movement(keyboard.w, keyboard.a, keyboard.s, keyboard.d);
+
+    player2.movement(keyboard.up, keyboard.left, keyboard.down, keyboard.right);
+
+    enemy1.movement();
+    enemy2.movement();
+    enemy3.movement();
+
+    enemy1.movement();
+    enemy2.movement();
+    enemy3.movement();
 }
