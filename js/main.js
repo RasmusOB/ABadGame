55e935bfc83ea6bb47604eda2604f5a7346ab6a3
Rasmus Olofsson <81038309+RasmusOB@users.noreply.github.com>
2021-10-28 06:42:42 +0000

    Added comments and stuff.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+// SETS THE FRAMERATE
 updatesPerSecond = 60;
 
+// CREATING VARIABLES
 let BG_COLOR = '#333';
 
 let playerCount = 0;
@@ -11,13 +13,14 @@ let enemy1 = new Enemy(500, 300);
 let enemy2 = new Enemy(500, 100);
 let enemy3 = new Enemy(500, 200);
 
-let player1 = new Player(100, 100, 'blue', pName1);
-let player2 = new Player(100, 100, 'purple', pName2);
+let player1 = new Player(500 - 400, 100, 'blue', pName1);
+let player2 = new Player(500 + 400, 100, 'purple', pName2);
 
 let time = 0;
 
+// FUNCTION THAT RUNS LIKE A LOOP THROUGH THE PROGRAM
 function update() {
-    logic();
+  logic();
 
-    graphics();
+  graphics();
 }
